a3775ff7636df037ffdddbf3091316f521250ce5
Michael Teixeira <meixeira@gmail.com>
2024-09-05 15:17:49 -0400

    • update current row on row completion • disable inputs based on current row • border styling based on current row

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import clone from "rfdc/default";
 import "./App.css";
 const allowedSymbols = [
@@ -18,6 +18,7 @@ const allowedSymbols = [
   "/",
 ];
 function App() {
+  const [currentRow, setCurrentRow] = useState(0);
   const [answerGrid, setAnswerGrid] = useState(
     new Array(6).fill(
       new Array(6).fill({
@@ -27,7 +28,7 @@ function App() {
       })
     )
   );
-  const handleChange = (row, column, event) => {
+  const handleSymbolInput = (row, column, event) => {
     const symbol = event.target.value;
     if (symbol !== "" && !allowedSymbols.includes(symbol)) return;
     let tempGrid = clone(answerGrid);
@@ -35,19 +36,45 @@ function App() {
     setAnswerGrid(tempGrid);
   };
 
+  const evaluateAnswer = () => {
+    //tbd
+  };
+
+  useEffect(() => {
+    const rowAnswered = answerGrid[currentRow].every((answer) => {
+      return !!answer.symbol;
+    });
+    if (rowAnswered) {
+      evaluateAnswer();
+      setCurrentRow(currentRow + 1);
+    }
+  }, [currentRow, answerGrid, setAnswerGrid]);
+
   return (
     <>
       <div>
         {answerGrid.map((row, i) => (
-          <div key={i} className="flex flex-row">
-            {row.map((item, j) => (
+          <div
+            key={i}
+            className={`flex flex-row border-4 rounded-[10px] ${
+              currentRow === i ? "border-white" : "border-transparent"
+            } transition-all`}
+          >
+            {row.map((answer, j) => (
               <div key={j} className="m-5 color-black">
                 <input
-                  className="bg-green-800 text-black text-[40px] text-center align-middle w-[80px] h-[80px] "
+                  className={`${
+                    answer.correctPlacement && answer.correctSymbol
+                      ? "bg-green-800"
+                      : answer.correctSymbol
+                      ? "bg-yellow-800"
+                      : "bg-black"
+                  } text-white text-[40px] text-center align-middle w-[80px] h-[80px]`}
                   maxLength="1"
                   type="text"
-                  value={item.symbol}
-                  onChange={(e) => handleChange(i, j, e)}
+                  value={answer.symbol}
+                  onChange={(e) => handleSymbolInput(i, j, e)}
+                  disabled={i !== currentRow}
                 />
               </div>
             ))}
