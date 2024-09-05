dc75f960c54f17b9cb35e56dcf2402db02be3e70
Michael Teixeira <meixeira@gmail.com>
2024-09-05 13:17:43 -0400

    Add grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const answerGrid = new Array(6).fill(
+    new Array(6).fill({
+      symbol: "x",
+      correctSymbol: false,
+      correctPlacement: false,
+    })
+  );
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {answerGrid.map((row, i) => (
+          <div key={i} className="flex flex-row">
+            {row.map((item, j) => (
+              <div key={j} className="color-white">
+                {item.symbol}
+              </div>
+            ))}
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
